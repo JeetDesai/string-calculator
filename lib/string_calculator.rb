1c5492a5687fa31dee7f30d9e0de8436356dccd2
Jeet Desai <jeetdesai32800@gmail.com>
2024-07-02 17:14:08 +0530

    String calculator final code refactoring

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,13 +1,22 @@
 class StringCalculator
   def self.add(string)
-    return 0 if string.empty?
-
     # Regular expression to match numbers, treating all non-digit characters as separators
-    numbers = string.scan(/-?\d+/).map(&:to_i)
+    numbers = string.empty? ? [] : string.scan(/-?\d+/).map(&:to_i)
 
     # Find all negative numbers
     negatives = numbers.select { |n| n < 0 }
     raise("negative numbers not allowed: #{negatives.join(', ')}") unless negatives.empty?
-    numbers.inject(0, :+)
+
+    # This can be return in single line but I have added puts to show results.
+    result = numbers.inject(0, :+)
+    puts "Adding numbers #{string.inspect} will give results = #{result}"
+    result
   end
 end
+
+StringCalculator.add("")
+StringCalculator.add("6")
+StringCalculator.add("1,2,6")
+StringCalculator.add("1\n2,6")
+StringCalculator.add("//;\n1;2")
+StringCalculator.add("//;\n1;2(4,5^2")
