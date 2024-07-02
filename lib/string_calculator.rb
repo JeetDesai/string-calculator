d2e571ed42c09bbee4f6ee3caede21634165f4ed
Jeet Desai <jeetdesai32800@gmail.com>
2024-07-02 15:16:05 +0530

    Consider new line character in core logic of add method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,6 @@
 class StringCalculator
   def self.add(string)
     return 0 if string.empty?
-    string.split(',').map(&:to_i).inject(:+)
+    string.split(/,|\n/).map(&:to_i).inject(:+)
   end
 end
-
-
