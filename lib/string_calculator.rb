a4f24fef9fe2e190a868354c15ee2d41cf0162da
Jeet Desai <jeetdesai32800@gmail.com>
2024-07-02 17:23:58 +0530

    Business Logic for numbers greater then 1000

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -8,6 +8,7 @@ class StringCalculator
     raise("negative numbers not allowed: #{negatives.join(', ')}") unless negatives.empty?
 
     # This can be return in single line but I have added puts to show results.
+    numbers.select!{|num| num <= 1000 }
     result = numbers.inject(0, :+)
     puts "Adding numbers #{string.inspect} will give results = #{result}"
     result
@@ -20,3 +21,4 @@ StringCalculator.add("1,2,6")
 StringCalculator.add("1\n2,6")
 StringCalculator.add("//;\n1;2")
 StringCalculator.add("//;\n1;2(4,5^2")
+StringCalculator.add("1001, 2, 3000")
